e1e8bdb1efbc2bc3bc58884b07c398be2acc1c83
Cool <ozzz.grisha@yandex.ru>
2021-07-29 13:59:39 +0300

    Убрал метод

--- app/src/main/java/com/pgh/my/MainActivity.kt
+++ app/src/main/java/com/pgh/my/MainActivity.kt
@@ -1,18 +1,9 @@
 package com.pgh.my
 
-
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
 }
